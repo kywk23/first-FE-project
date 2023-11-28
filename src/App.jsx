7d7cebac3e445e8e5c3454f3e53c384a4c2e6c84
kywk23 <kelvinywk.work@outlook.com>
2023-11-28 15:30:30 +0800

    moneytree + buttons UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import logo from "/logo.png";
 import "./App.css";
+import React from "react";
+import { Container, Navbar, Button } from "react-bootstrap";
+import MoneyTree from "./moneytree";
+import questions from "./questions";
 
 function App() {
   return (
     <>
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      <h1>Project 1</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="section">
+        <div className="logo">
+          <img src="WWTBAMUS2020Logo.png" alt="WWTBAM image"></img>
+        </div>
+        {/* <h1>Who wants to be a Millionaire</h1> */}
+        <div className="lifeline">
+          <ul className="lifeline-buttons">
+            <li>
+              <Button variant="success">50:50</Button>
+            </li>
+            <li>
+              <Button variant="outline-primary">Phone a Friend</Button>
+            </li>
+            <li>
+              <Button variant="outline-primary">Audience</Button>
+            </li>
+          </ul>
+        </div>
+        <div className="moneytree">
+          <MoneyTree />
+        </div>
       </div>
     </>
   );
---
+++ src/moneytree.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./App.css";
+
+const MoneyTree = () => {
+  return (
+    <>
+      <p>
+        <span>15</span> $1,000,000
+      </p>
+      <p>
+        <span>14</span> $500,000
+      </p>
+      <p>
+        <span>13</span> $250,000
+      </p>
+      <p>
+        <span>12</span> $125,000
+      </p>
+      <p>
+        <span>11</span> $64,000
+      </p>
+      <p>
+        <span>10</span> $32,000
+      </p>
+      <p>
+        <span>9</span> $16,000
+      </p>
+      <p>
+        <span>8</span> $8,000
+      </p>
+      <p>
+        <span>7</span> $4,000
+      </p>
+      <p>
+        <span>6</span> $2,000
+      </p>
+      <p>
+        <span>5</span> $1,000
+      </p>
+      <p>
+        <span>4</span> $500
+      </p>
+      <p>
+        <span>3</span> $300
+      </p>
+      <p>
+        <span>2</span> $200
+      </p>
+      <p>
+        <span>1</span> $100
+      </p>
+    </>
+  );
+};
+
+export default MoneyTree;
